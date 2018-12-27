0f690453988a150f485d3b9942b8717823d8a139
progranime <jeremyespinosa1995@gmail.com>
2018-12-27 16:10:31 +0800

    added page title

--- client/src/routes/OnlineCode/index.js
+++ client/src/routes/OnlineCode/index.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import OnlineCode from '../../containers/OnlineCode'
 
 export class Index extends Component {
+    componentDidMount() {
+        window.document.title = `Jeremy's Portfolio - Online Code`
+    }
     render() {
         return (
             <div className="main-container">
