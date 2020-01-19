0df59aaa9133214afc893a8e5e02152b7dda0dd3
progranime <jeremyespinosa1995@gmail.com>
2020-01-19 16:42:53 +0800

    added offset to the scroll bottom listener

--- client/src/actions/portfolioActions.js
+++ client/src/actions/portfolioActions.js
@@ -1,71 +1,72 @@
-import axios from 'axios'
+import axios from "axios"
 
-import store from '../store'
+import store from "../store"
 import {
-    GET_PORTFOLIOS,
-    GET_PORTFOLIO,
-    GET_MORE_PORTFOLIOS,
-    RESET_PORTFOLIOS
-} from './types'
+  GET_PORTFOLIOS,
+  GET_PORTFOLIO,
+  GET_MORE_PORTFOLIOS,
+  RESET_PORTFOLIOS
+} from "./types"
 
 export const getPortfolios = () => dispatch => {
-    const axiosOptions = {
-        method: 'get',
-        url: `/api/portfolio`
-    }
+  const axiosOptions = {
+    method: "get",
+    url: `/api/portfolio`
+  }
 
-    axios(axiosOptions).then(res => {
-        dispatch({
-            type: GET_PORTFOLIOS,
-            payload: {
-                results: res.data
-            }
-        })
+  axios(axiosOptions).then(res => {
+    console.log("Data: ", res.data)
+    dispatch({
+      type: GET_PORTFOLIOS,
+      payload: {
+        results: res.data
+      }
     })
+  })
 }
 
 export const getPortfolio = payload => dispatch => {
-    const axiosOptions = {
-        method: 'get',
-        url: `/api/portfolio/${payload.id}`
-    }
+  const axiosOptions = {
+    method: "get",
+    url: `/api/portfolio/${payload.id}`
+  }
 
-    axios(axiosOptions).then(res => {
-        dispatch({
-            type: GET_PORTFOLIO,
-            payload: {
-                result: res.data
-            }
-        })
+  axios(axiosOptions).then(res => {
+    dispatch({
+      type: GET_PORTFOLIO,
+      payload: {
+        result: res.data
+      }
     })
+  })
 }
 
 export const getMorePortfolios = payload => dispatch => {
-    const page = payload.page
+  const page = payload.page
 
-    const axiosOptions = {
-        method: 'get',
-        url: `/api/portfolio/page/${page}`
-    }
+  const axiosOptions = {
+    method: "get",
+    url: `/api/portfolio/page/${page}`
+  }
 
-    axios(axiosOptions).then(res => {
-        // concat the previous data
-        let results = store.getState().portfolio.results.concat(res.data)
+  axios(axiosOptions).then(res => {
+    // concat the previous data
+    let results = store.getState().portfolio.results.concat(res.data)
 
-        dispatch({
-            type: GET_MORE_PORTFOLIOS,
-            payload: {
-                results
-            }
-        })
+    dispatch({
+      type: GET_MORE_PORTFOLIOS,
+      payload: {
+        results
+      }
     })
+  })
 }
 
 export const resetPortfolios = payload => dispatch => {
-    dispatch({
-        type: RESET_PORTFOLIOS,
-        payload: {
-            results: []
-        }
-    })
+  dispatch({
+    type: RESET_PORTFOLIOS,
+    payload: {
+      results: []
+    }
+  })
 }
--- client/src/containers/Portfolio/List/index.js
+++ client/src/containers/Portfolio/List/index.js
@@ -1,104 +1,107 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import BottomScrollListener from 'react-bottom-scroll-listener'
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import BottomScrollListener from "react-bottom-scroll-listener"
 
 import {
-    getPortfolios,
-    getMorePortfolios,
-    resetPortfolios
-} from '../../../actions/portfolioActions'
-import { CardPortfolio, Spinner, ScrollTo } from '../../../components'
-import CardPortfolioWrapper from '../../../components/Card/Portfolio/Wrapper'
+  getPortfolios,
+  getMorePortfolios,
+  resetPortfolios
+} from "../../../actions/portfolioActions"
+import { CardPortfolio, Spinner, ScrollTo } from "../../../components"
+import CardPortfolioWrapper from "../../../components/Card/Portfolio/Wrapper"
 
 class Index extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            page: 1
-        }
+    this.state = {
+      page: 1
     }
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    this.props.getMorePortfolios({
+      page: this.state.page
+    })
+  }
+
+  handleGetMorePortfolios = () => {
+    this.setState(
+      {
+        page: this.state.page + 1
+      },
+      () => {
         this.props.getMorePortfolios({
-            page: this.state.page
+          page: this.state.page
         })
-    }
+      }
+    )
+  }
 
-    handleGetMorePortfolios = () => {
-        this.setState(
-            {
-                page: this.state.page + 1
-            },
-            () => {
-                this.props.getMorePortfolios({
-                    page: this.state.page
-                })
-            }
-        )
-    }
+  renderCardPortfolio() {
+    const { portfolio } = this.props
 
-    renderCardPortfolio() {
-        const { portfolio } = this.props
+    const portfolios = portfolio.results.map(portfolio => {
+      return (
+        <CardPortfolio
+          key={portfolio._id}
+          id={portfolio._id}
+          title={portfolio.title}
+          description={portfolio.description}
+          position={portfolio.position}
+          task={portfolio.task}
+          technology={portfolio.technology.split(",")}
+          gallery={portfolio.gallery}
+        />
+      )
+    })
 
-        const portfolios = portfolio.results.map(portfolio => {
-            return (
-                <CardPortfolio
-                    key={portfolio._id}
-                    id={portfolio._id}
-                    title={portfolio.title}
-                    description={portfolio.description}
-                    position={portfolio.position}
-                    task={portfolio.task}
-                    technology={portfolio.technology.split(',')}
-                    gallery={portfolio.gallery}
-                />
-            )
-        })
+    return <CardPortfolioWrapper>{portfolios}</CardPortfolioWrapper>
+  }
 
-        return <CardPortfolioWrapper>{portfolios}</CardPortfolioWrapper>
-    }
+  componentWillUnmount() {
+    this.props.resetPortfolios()
+  }
 
-    componentWillUnmount() {
-        this.props.resetPortfolios()
-    }
+  render() {
+    const { portfolio } = this.props
 
-    render() {
-        const { portfolio } = this.props
-
-        return (
-            <BottomScrollListener onBottom={this.handleGetMorePortfolios}>
-                <ScrollTo>
-                    {portfolio.results.length ? (
-                        this.renderCardPortfolio()
-                    ) : (
-                        <Spinner size="md" />
-                    )}
-                </ScrollTo>
-            </BottomScrollListener>
-        )
-    }
+    return (
+      <BottomScrollListener
+        onBottom={this.handleGetMorePortfolios}
+        offset="500"
+      >
+        <ScrollTo>
+          {portfolio.results.length ? (
+            this.renderCardPortfolio()
+          ) : (
+            <Spinner size="md" />
+          )}
+        </ScrollTo>
+      </BottomScrollListener>
+    )
+  }
 }
 
 Index.propTypes = {
-    portfolio: PropTypes.object.isRequired,
-    getPortfolios: PropTypes.func.isRequired,
-    getMorePortfolios: PropTypes.func.isRequired,
-    resetPortfolios: PropTypes.func.isRequired
+  portfolio: PropTypes.object.isRequired,
+  getPortfolios: PropTypes.func.isRequired,
+  getMorePortfolios: PropTypes.func.isRequired,
+  resetPortfolios: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-    portfolio: state.portfolio
+  portfolio: state.portfolio
 })
 
 const mapDispatchToProps = {
-    getPortfolios,
-    getMorePortfolios,
-    resetPortfolios
+  getPortfolios,
+  getMorePortfolios,
+  resetPortfolios
 }
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Index)
